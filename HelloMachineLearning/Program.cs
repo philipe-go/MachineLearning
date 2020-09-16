3e20e0be2c2e4fbba765f37689dbe3c563f533ba
Phil[go] <philipe.ng@protonmail.com>
2020-09-16 17:10:40 -0400

    HelloMachineLearning Console App

---
+++ HelloMachineLearning/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using HelloMachineLearningML.Model;
+
+namespace HelloMachineLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Machine Learning!\n");
+            Console.WriteLine("Enter your text below:");
+
+            // Add input data
+            var input = new ModelInput();
+            input.SentimentText = Console.ReadLine();
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}");
+        }
+    }
+}
